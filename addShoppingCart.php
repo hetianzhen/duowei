b246d4ff5d49176b42a15cac3527b02ffe098b12
hetianzhen <1085965745@qq.com>
2018-01-25 21:50:46 +0800

    1.25晚上

--- addShoppingCart.php
+++ addShoppingCart.php
@@ -3,9 +3,11 @@
 	header("Content-Type:text/html;charset=utf-8");
 	//1、接受客户端的数据（用户输入的数据）
 	$vipName   = $_REQUEST['vipName'];
+	$goodsId   = $_REQUEST['goodsId'];
 	$goodsCount = $_REQUEST['goodsCount'];
 	$goodsImg = $_REQUEST['goodsImg'];
 	$goodsName = $_REQUEST['goodsName'];
+	// $beiyong1 = $_REQUEST['beiyong1'];
 	$goodsPrice = $_REQUEST['goodsPrice'];
 	
 	//2、数据保存在数据库中
@@ -18,24 +20,25 @@
 	};
 	
 	//3）、传输数据（过桥）
-	$result = mysql_query("select * from shoppingCart where vipName='".$vipName."' and goodsCount='".$goodsCount."' and goodsImg='".$goodsImg."' and goodsName='".$goodsName."' and goodsPrice='".$goodsPrice."'",$conn);
+	$sqlstr="select * from shoppingCart where vipName='".$vipName."' and goodsImg='".$goodsId."' and goodsCount='".$goodsCount."' and goodsImg='".$goodsImg."' and goodsName='".$goodsName."' and goodsPrice='".$goodsPrice."'";
+	//echo $sqlstr;
+	$result = mysql_query($sqlstr,$conn);
+
 	//3.1)先查找该商品是否在购物车中存在
 	if(mysql_num_rows($result)>0){
 		//如果存在，则使用update语句
 		$sqlstr = "update shoppingCart set goodsCount=goodsCount+".$goodsCount." where vipName='".$vipName."'";
 	}else{
 		//如果不存在，则使用insert语句	
-		$sqlstr = "insert into shoppingCart values('".$vipName."','".$goodsCount."','".$goodsImg."','".$goodsName."','".$goodsPrice."')";		
+		$sqlstr = "insert into shoppingCart values('".$vipName."','".$goodsId."','".$goodsCount."','".$goodsImg."','".$goodsName."','".$goodsPrice."')";		
 	}
 	
-	$result=mysql_query($sqlstr,$conn);	
+	mysql_query($sqlstr,$conn);
 	//4）、关闭连接（拆桥）
 	mysql_close($conn);
-	
 	// if(!$result){
 	// 	die("添加失败".mysql_error());
 	// }	
-	
 	//3、给客户端返回（响应） 1：表示添加成功 0：表示添加失败
 	if($result>0){
 		echo 1;	
